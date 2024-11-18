9da06e35fe6efc45f9327118c7c85adf66216fd2
Prateek Kesarwani <KesarwaniP@cardiff.ac.uk>
2024-11-18 11:03:14 +0000

    Initial commit

---
+++ src/main/java/com/team2/sportsleague/SportsleagueApplication.java
@@ -0,0 +1,13 @@
+package com.team2.sportsleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsleagueApplication.class, args);
+	}
+
+}
