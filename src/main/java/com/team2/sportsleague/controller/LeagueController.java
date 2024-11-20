04ea496b13def6c1f361f8f8cef3318d80ce1bea
Taha Murtaza Ali <alitm@cardiff.ac.uk>
2024-11-20 19:02:08 +0000

    Dynamically showing Match Fixture with correct positioning

--- src/main/java/com/team2/sportsleague/controller/LeagueController.java
+++ src/main/java/com/team2/sportsleague/controller/LeagueController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 @Controller
@@ -53,6 +52,58 @@ public class LeagueController {
     public ModelAndView showMatch(){
         ModelAndView mvc = new ModelAndView("match");
 
+        // List to hold round data
+        List<List<Map<String, String>>> rounds = new ArrayList<>();
+
+        // Round 1
+        List<Map<String, String>> round1 = new ArrayList<>();
+        Map<String, String> match1 = new HashMap<>();
+        match1.put("team1", "Team Red");
+        match1.put("team2", "Team Blue");
+        round1.add(match1);
+
+        Map<String, String> match2 = new HashMap<>();
+        match2.put("team1", "Team Yellow");
+        match2.put("team2", "Team Black");
+        round1.add(match2);
+
+        Map<String, String> match3 = new HashMap<>();
+        match3.put("team1", "Team White");
+        match3.put("team2", "Team Pink");
+        round1.add(match3);
+
+        Map<String, String> match4 = new HashMap<>();
+        match4.put("team1", "Team Green");
+        match4.put("team2", "Team Indigo");
+        round1.add(match4);
+
+        rounds.add(round1);
+
+        // Round 2
+        List<Map<String, String>> round2 = new ArrayList<>();
+        Map<String, String> match5 = new HashMap<>();
+        match5.put("team1", "Team Red");
+        match5.put("team2", "Team Black");
+        round2.add(match5);
+
+        Map<String, String> match6 = new HashMap<>();
+        match6.put("team1", "Team White");
+        match6.put("team2", "Team Indigo");
+        round2.add(match6);
+
+        rounds.add(round2);
+
+        // Round 3
+        List<Map<String, String>> round3 = new ArrayList<>();
+        Map<String, String> match7 = new HashMap<>();
+        match7.put("team1", "Team Red");
+        match7.put("team2", "Team Indigo");
+        round3.add(match7);
+
+        rounds.add(round3);
+
+        // Pass data to the view
+        mvc.addObject("rounds", rounds);
         return mvc;
     }
 }
