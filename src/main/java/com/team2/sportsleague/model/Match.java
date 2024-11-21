2971977fbf3aa405b1db873cbcf96f4c1e438713
Taha Murtaza Ali <alitm@cardiff.ac.uk>
2024-11-21 11:51:17 +0000

    Match Class now has relevant fields

--- src/main/java/com/team2/sportsleague/model/Match.java
+++ src/main/java/com/team2/sportsleague/model/Match.java
@@ -1,4 +1,16 @@
 package com.team2.sportsleague.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Match {
+
+    int playerId1;
+    int playerId2;
+    String playerName1;
+    String playerName2;
+    int playerScore1;
+    int playerScore2;
 }
