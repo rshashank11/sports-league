e0d90dc898c1f584ca843ddcd403ad04ce799e9f
Taha Murtaza Ali <alitm@cardiff.ac.uk>
2024-11-26 16:05:13 +0000

    Created Leagues table and linked it to matches

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,18 @@
 DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS leagues;
+
+CREATE TABLE leagues (
+                         league_id INT AUTO_INCREMENT PRIMARY KEY,   -- Unique ID for each league
+                         league_name VARCHAR(255) NOT NULL,           -- Name of the league (e.g., "Premier League")
+                         number_of_players INT,                      -- Number of players in the league
+                         start_date DATE,                             -- Start date of the league
+                         end_date DATE,                               -- End date of the league
+                         status VARCHAR(50)                           -- Status of the league (e.g., active, completed)
+)engine= InnoDB;
+
 
 CREATE TABLE matches (
+                         league_id INT,
                          match_id INT AUTO_INCREMENT PRIMARY KEY,
                          player1_id INT NOT NULL,
                          player2_id INT NOT NULL,
@@ -9,5 +21,6 @@ CREATE TABLE matches (
                          score_player1 INT NOT NULL,
                          score_player2 INT NOT NULL,
                          winner_id INT NOT NULL,
-                         round_number INT NOT NULL
+                         round_number INT NOT NULL,
+                         FOREIGN KEY (league_id) REFERENCES leagues(league_id)
 )engine=InnoDB;
