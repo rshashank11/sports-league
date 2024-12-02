bf29b82fbeeaf0c0fa41350f9bb33eedfa268fe6
Taha Murtaza Ali <alitm@cardiff.ac.uk>
2024-12-02 19:53:49 +0000

    Created MatchController class and removed GetMapping from LeagueController

---
+++ src/main/java/com/team2/sportsleague/controller/MatchController.java
@@ -0,0 +1,36 @@
+package com.team2.sportsleague.controller;
+
+import com.team2.sportsleague.model.Round;
+import com.team2.sportsleague.repository.MatchRepository;
+import com.team2.sportsleague.service.LeagueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+
+@Controller
+public class MatchController {
+
+    private MatchRepository matchRepository;
+
+    @Autowired
+    public MatchController(MatchRepository matchRepository) {
+        this.matchRepository = matchRepository;
+    }
+
+
+    // Show match page
+    @GetMapping("/match")
+    public ModelAndView showMatch() {
+        ModelAndView mvc = new ModelAndView("match");
+
+        List<Round> rounds = matchRepository.getAllRounds();
+
+        // Pass data to the view
+        mvc.addObject("rounds", rounds);
+        return mvc;
+    }
+}
